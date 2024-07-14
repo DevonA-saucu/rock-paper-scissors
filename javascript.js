4fefaaf610d8ffc8c114a7565dcf039e8a2c81b1
Devon <60799686+DevonA-saucu@users.noreply.github.com>
2024-07-13 22:41:05 -0500

    Add UI to rock paper scissors

--- javascript.js
+++ javascript.js
@@ -29,85 +29,100 @@ function getHumanChoice() {
     return humChoice;
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
 
-    let counter = 0;
+let humanScore = 0;
+let computerScore = 0;
 
-    function playRound(humanChoice, computerChoice) {
-        console.log(humanChoice);
-        switch (humanChoice) {
-            case "rock":
-                switch (computerChoice) {
-                    case "paper":
-                        console.log("You lose! Paper beats rock!");
-                        computerScore++;
-                        break;
-                    case "scissors":
-                        console.log("You win! Rock beats scissors!");
-                        humanScore++
-                        break;
-                    case "rock":
-                        console.log("It's a tie! Neither rock wins!");
-                        break;
-                    default:
-                        console.log("Something is wrong!");
-                }
-                break;
-            case "paper":
-                switch (computerChoice) {
-                    case "paper":
-                        console.log("It's a tie! Neither paper wins!");
-                        break;
-                    case "scissors":
-                        console.log("You lose! Scissors beats paper!");
-                        computerScore++
-                        break;
-                    case "rock":
-                        console.log("You win! Paper beats rock!");
-                        humanScore++
-                        break;
-                    default:
-                        console.log("Something is wrong!");
-                }
-                break;
-            case "scissors":
-                switch (computerChoice) {
-                    case "paper":
-                        console.log("You win! Scissors beats paper!");
-                        humanScore++
-                        break;
-                    case "scissors":
-                        console.log("It's a tie! Neither scissors wins!");
-                        break;
-                    case "rock":
-                        console.log("You lose! Rock beats scissors!");
-                        computerScore++
-                        break;
-                    default:
-                        console.log("Something is wrong!");
-                }
-                break;
-            default:
-                console.log("Something is wrong!");
+let counter = 0;
+
+const buttons = document.querySelector(".buttons");
+const results = document.querySelector(".results");
+
+buttons.addEventListener("click", (e) => {
+    let target = e.target;
+
+    playRound(target.className, getComputerChoice());
+});
+
+function playRound(humanChoice, computerChoice) {
+    results.textContent = humanChoice;
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "paper":
+                    computerScore++;
+                    results.textContent = `You lose! Paper beats rock! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                case "scissors":
+                    humanScore++
+                    results.textContent = `You win! Rock beats scissors! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                case "rock":
+                    results.textContent = `It's a tie! Neither rock wins! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                default:
+                    results.textContent = `Something is wrong!`;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "paper":
+                    results.textContent = `It's a tie! Neither paper wins! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                case "scissors":
+                    computerScore++
+                    results.textContent = `You lose! Scissors beats paper! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                case "rock":
+                    humanScore++
+                    results.textContent = `You win! Paper beats rock! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                default:
+                    results.textContent = `Something is wrong!`;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {   
+                case "paper":
+                    humanScore++
+                    results.textContent = `You win! Scissors beats paper! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                case "scissors":
+                    results.textContent = `It's a tie! Neither scissors wins! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                case "rock":
+                    computerScore++
+                    results.textContent = `You lose! Rock beats scissors! Score is, Human: ${humanScore} Computer: ${computerScore}`;
+                    counter++;
+                    break;
+                default:
+                    results.textContent = `Something is wrong!`;
+            }
+            break;
+        default:
+            results.textContent = "Something is wrong!";
+    }
+    if (counter === 5) {
+        if (humanScore > computerScore) {
+            results.textContent = `You won the game! Your score was ${humanScore} and the computer's was ${computerScore}!`;
         }
-    }
+        else if (computerScore > humanScore) {
+            results.textContent = `You lost the game! Your score was ${humanScore} and the computer's was ${computerScore}!`;
+        }
+        else {
+            results.textContent = `You tied the game! You and the computer both had a score of ${humanScore}!`;
+        }
 
-    while (counter !== 5) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-        counter++;
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`You won! Your score was ${humanScore} and the computer's was ${computerScore}!`);
-    }
-    else if (computerScore > humanScore) {
-        console.log(`You lost! Your score was ${humanScore} and the computer's was ${computerScore}!`);
-    }
-    else {
-        console.log(`You tied! You and the computer both had a score of ${humanScore}!`);
+        humanScore = 0;
+        computerScore = 0;
+        counter = 0;
     }
 }
